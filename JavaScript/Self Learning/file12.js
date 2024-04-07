6a0cc5e263eb72f85cc678d5af1269c97b0b58f3
faizanpasha356 <mdalifaizan356@gmail.com>
2024-04-07 23:42:49 +0530

    learn object in js

--- JavaScript/Self Learning/file12.js
+++ JavaScript/Self Learning/file12.js
@@ -11,19 +11,36 @@
 
 "use strict";
 
-//Iterate using for in loop
+// //Iterate using for in loop
+//     const person ={
+//         Name: "Faizan",
+//         Age: 26,
+//         "Person hobby":["Coding", "Sleeping", "Music"]
+//     };
+
+//     for(let key in person){
+//         // console.log(key);                      //Print all key
+//         // console.log(person["key"]);           //undefined
+//         // console.log(person[key]);            //Print values
+//         // console.log(key, "=", person[key]);  //Print key values pair
+//         console.log(`${key}=${person[key]}`);  //Print key values pair usin template string
+//     }
+
+
+//Iterate using object.keys
     const person ={
         Name: "Faizan",
         Age: 26,
         "Person hobby":["Coding", "Sleeping", "Music"]
     };
 
-    for(let key in person){
-        // console.log(key);                      //Print all key
-        // console.log(person["key"]);           //undefined
-        // console.log(person[key]);            //Print values
-        // console.log(key, "=", person[key]);  //Print key values pair
-        console.log(`${key}=${person[key]}`);  //Print key values pair usin template string
-    }
+    console.log(Object.keys(person));
+    console.log(typeof(Object.keys(person)));
+    const val=Array.isArray(Object.keys(person));
+    console.log(val);
 
+    for(let key of Object.keys(person)){
+        console.log(person[key]);
+    }
+    
 
