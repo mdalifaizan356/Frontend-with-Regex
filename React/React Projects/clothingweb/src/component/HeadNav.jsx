6f9d67149a26ca4c9dd3bac5753cc825ebfc8278
faizanpasha356 <mdalifaizan356@gmail.com>
2024-04-30 23:06:09 +0530

    Start new project

---
+++ React/React Projects/clothingweb/src/component/HeadNav.jsx
@@ -0,0 +1,21 @@
+import '../style/HeadNav.css'
+
+function HeadNav() {
+  return (
+    <>
+    <div className='container'>
+        <div className='logo'>SixteenClothing</div>
+        <div className='head-Navigation'>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Our Products</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default HeadNav
