4e1de8fec3382cd946adcbf88ed9f0f66ce44aec
faizanpasha356 <mdalifaizan356@gmail.com>
2024-05-23 23:22:10 +0530

    Practice

--- React/React Projects/my_first_app/src/Component/Header/Header.jsx
+++ React/React Projects/my_first_app/src/Component/Header/Header.jsx
@@ -3,7 +3,14 @@ function Header() {
     return (
       <>
       <div className='Header-Container'>
-        <h1>I am Header</h1>
+        <div className='headnav'>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">Our Products</a></li>
+          </ul>
+        </div>
       </div>
       </>
     )
---
+++ React/React Projects/my_first_app/src/Pages/About/about.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Component/Header/Header'
+import ContentContainer from './Component/ContentContainer/ContentContainer'
+import Footer from './Component/Footer/Footer'
+
+function App() {
+  return (
+    <>
+    <Header />
+
+    <Footer />
+    </>
+  )     
+}
+
+export default App
