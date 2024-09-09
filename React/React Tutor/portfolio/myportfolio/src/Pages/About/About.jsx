ee2dbb4c0e5fa5700aef7c362455416864bfdf3d
faizanpasha356 <mdalifaizan356@gmail.com>
2024-09-09 23:42:02 +0530

    Learn React

---
+++ React/React Tutor/portfolio/myportfolio/src/Pages/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+
+const About = () => {
+  return (
+    <>
+      <Header />
+      <div>About Me</div>
+    </>
+  )
+}
+
+export default About
---
+++ React/React Tutor/portfolio/myportfolio/src/Pages/WICD/WICD.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+
+const WICD = () => {
+  return (
+    <>
+      <Header />
+      <div>What I Do</div>
+    </>
+  )
+}
+
+export default WICD
